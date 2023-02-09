661fb19ace1cb091b96e12b2d8803b42ebc6913c
tlaloc2002 <88161866+tlaloc2002@users.noreply.github.com>
2023-02-09 12:27:07 -0800

    started contentScript and background

--- background.js
+++ background.js
@@ -1,12 +1,16 @@
+// Listener that listens to tabs, will listen for an update to tabs
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
+  // See if there's a tab URL, and if there is, see if it includes youtube.com/watch since that is in every youtube video URL
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    // Grab the value that comes after the watch?v= portion of URL
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    // Send a message to contentScript saying a new video has loaded and this is the videoId(unique value of the URL) of that video
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v")
+    });
+  }
   
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+})
